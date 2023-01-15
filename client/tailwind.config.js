24122fe8ee5473fec3add453f84517a348744657
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-15 18:58:56 +0800

    Initialize repository

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        'space': ['Space Grotesk'],
+      },
+
+      colors: {
+        'blk': '#1E1E1E',
+        'wht': '#F9F9F9',
+        'orng': '#FC5F1C',
+        'red': '#F63131',
+        'light-orng': '#FF8A58',
+        'light-gry': '#838383',
+        'light-lgry': '#D9D9D9'
+      },
+    },
+  },
+  plugins: [],
+}
